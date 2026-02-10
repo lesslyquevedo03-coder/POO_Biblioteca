5a3a13e2a4595a1b34d2dfdf558049f155225ad7
Lesly <lesslyquevedo03@gmail.com>
2026-02-10 13:25:57 +0100

    Terminada implementacóon de la clase main

--- src/com/Ercilla/BibliotecaLibros.java
+++ src/com/Ercilla/BibliotecaLibros.java
@@ -2,6 +2,20 @@ package com.Ercilla;
 
 public class BibliotecaLibros {
     public static void main(String[] args) {
+        //Variable biblioteca de la clase Biblioteca
+        Biblioteca biblioteca = new Biblioteca();
 
+        biblioteca.agregarLibro(new Terror("Stephen King", "It", 9));
+        biblioteca.agregarLibro(new CienciaFiccion("Asimov", "Fundación", "Trantor"));
+        biblioteca.agregarLibro(new NovelaNegra("Agatha Cristie", "Diez negritos", "Wargrave"));
+        biblioteca.agregarLibro(new NovelaRomantica("Jane Austen", "Orgullo y Prejuicio", true));
+
+        biblioteca.mostrarLibros();
+
+        System.out.println("_______Préstamos______");
+        biblioteca.prestarLirbos();
+
+        System.out.println("_________Prestar otra vez_________");
+        biblioteca.prestarLirbos();
     }
 }
